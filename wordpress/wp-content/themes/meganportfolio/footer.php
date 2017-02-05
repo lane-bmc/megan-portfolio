10037ac7b77b1bffb663b56694a54dbd1026fac4
lane-bmc <lane.bmc@gmail.com>
2017-02-04 22:42:20 -0800

    Made Instagram and Behance footer links dynamically alterable from WP

--- wordpress/wp-content/themes/meganportfolio/footer.php
+++ wordpress/wp-content/themes/meganportfolio/footer.php
@@ -3,8 +3,8 @@
       <footer>
         <p><a href="mailto:megan.jk.lane@gmail.com">megan.jk.lane@gmail.com</a></p>
         <p><a href="#">resume</a><br/>
-          <a href="https://www.instagram.com/singing_in_the_lane/" target="_blank">instagram</a><br/>
-          <a href="https://www.behance.net/meganjklane879" target="_blank">behance</a></p>
+          <a href="<?php echo get_option('instagram'); ?>" target="_blank">instagram</a><br/>
+          <a href="<?php echo get_option('behance'); ?>" target="_blank">behance</a></p>
           </div>
       </footer>
     </div>
--- wordpress/wp-content/themes/meganportfolio/functions.php
+++ wordpress/wp-content/themes/meganportfolio/functions.php
@@ -9,4 +9,43 @@ add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
 // Support Featured Images
 add_theme_support( 'post-thumbnails' );
 
+// Custom settings
+function custom_settings_add_menu() {
+  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99 );
+}
+add_action( 'admin_menu', 'custom_settings_add_menu' );
+
+// Create Custom Global Settings
+function custom_settings_page() { ?>
+  <div class="wrap">
+    <h1>Custom Settings</h1>
+    <form method="post" action="options.php">
+       <?php
+           settings_fields( 'section' );
+           do_settings_sections( 'theme-options' );      
+           submit_button(); 
+       ?>          
+    </form>
+  </div>
+<?php }
+
+// Instagram
+function setting_instagram() { ?>
+  <input type="text" name="instagram" id="instagram" value="<?php echo get_option( 'instagram' ); ?>" />
+<?php }
+
+function setting_behance() { ?>
+  <input type="text" name="behance" id="behance" value="<?php echo get_option( 'behance' ); ?>" />
+<?php }
+
+function custom_settings_page_setup() {
+  add_settings_section( 'section', 'All Settings', null, 'theme-options' );
+  add_settings_field( 'instagram', 'Instagram URL', 'setting_instagram', 'theme-options', 'section' );
+  add_settings_field( 'behance', 'Behance URL', 'setting_behance', 'theme-options', 'section' );
+
+  register_setting('section', 'instagram');
+  register_setting('section', 'behance');
+}
+add_action( 'admin_init', 'custom_settings_page_setup' );
+
 ?>
